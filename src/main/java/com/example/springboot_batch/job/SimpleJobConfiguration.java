89112632688563719da0f61cddb19cd614c39800
Lee SeungHwan <lsh01087674117@gmail.com>
2019-12-22 21:06:33 +0900

    리펙토링

--- src/main/java/com/example/springboot_batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/springboot_batch/job/SimpleJobConfiguration.java
@@ -5,15 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * @author 이승환
- * @since 2019/12/22
- */
 @Slf4j // log 사용을 위한 lombok 어노테이션
 @RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
 @Configuration
