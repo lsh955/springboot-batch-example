99306e559160fe7d62f06e5e1cff1c03b77b6b55
이승환 <knetdev@knetbiz.com>
2019-12-23 09:10:33 +0900

    주석입력

--- src/main/java/com/example/springboot_batch/SpringbootBatchApplication.java
+++ src/main/java/com/example/springboot_batch/SpringbootBatchApplication.java
@@ -4,7 +4,7 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableBatchProcessing
+@EnableBatchProcessing  // 배치기능 활성화
 @SpringBootApplication
 public class SpringbootBatchApplication {
 
