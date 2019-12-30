cd48f881cc917007b4f2f9774e1bc95ece0843d4
이승환 <lsh01087674117@gmail.com>
2019-12-30 16:12:14 +0900

    배치코드 수정

--- src/main/java/com/example/springboot_batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/springboot_batch/job/SimpleJobConfiguration.java
@@ -19,16 +19,16 @@ public class SimpleJobConfiguration {
 
     @Bean
     public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob")
+        return jobBuilderFactory.get("simpleJob")   // simpleJob 이란 이름의 Batch Job을 생성
                 .start(simpleStep1())
                 .build();
     }
 
     @Bean
     public Step simpleStep1() {
-        return stepBuilderFactory.get("simpleStep1")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info(">>>>> This is Step1");
+        return stepBuilderFactory.get("simpleStep1")    // simpleStep1 이란 이름의 Batch Step을 생성
+                .tasklet((contribution, chunkContext) -> {  // Step 안에서 수행될 기능들을 명시(Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할때 사용)
+                    log.info(">>>>> This is Step1");    // Batch가 수행되면 log.info(">>>>> This is Step1") 가 출력
                     return RepeatStatus.FINISHED;
                 })
                 .build();
