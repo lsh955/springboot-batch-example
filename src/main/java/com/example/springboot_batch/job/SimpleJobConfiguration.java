329f924d7b6cbdd8537bf55ef08ce4573a4daa25
이승환 <knetdev@knetbiz.com>
2020-01-02 08:33:22 +0900

    정렬

--- src/main/java/com/example/springboot_batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/springboot_batch/job/SimpleJobConfiguration.java
@@ -12,33 +12,33 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j // log 사용을 위한 lombok 어노테이션
-@RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
+@Slf4j                      // log 사용을 위한 lombok 어노테이션
+@RequiredArgsConstructor    // 생성자 DI를 위한 lombok 어노테이션
 @Configuration
 public class SimpleJobConfiguration {
-    private final JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
-    private final StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음
-
+    private final JobBuilderFactory jobBuilderFactory;      // 생성자 DI 받음
+    private final StepBuilderFactory stepBuilderFactory;    // 생성자 DI 받음
+    
     @Bean
     public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob")   // simpleJob 이란 이름의 Batch Job을 생성
+        return jobBuilderFactory.get("simpleJob")           // simpleJob 이란 이름의 Batch Job을 생성
                 .start(simpleStep1(null))
                 .next(simpleStep2(null))
                 .build();
     }
-
+    
     @Bean
     @JobScope
     public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        return stepBuilderFactory.get("simpleStep1")    // simpleStep1 이란 이름의 Batch Step을 생성
+        return stepBuilderFactory.get("simpleStep1")        // simpleStep1 이란 이름의 Batch Step을 생성
                 .tasklet((contribution, chunkContext) -> {  // Step 안에서 수행될 기능들을 명시(Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할때 사용)
-                    log.info(">>>>> This is Step1");    // Batch가 수행되면 log.info(">>>>> This is Step1") 가 출력
+                    log.info(">>>>> This is Step1");        // Batch가 수행되면 log.info(">>>>> This is Step1") 가 출력
                     log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
     }
-
+    
     @Bean
     @JobScope
     public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
